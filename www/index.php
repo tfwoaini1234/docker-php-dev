e2d4e3c5a7efabb591179da941f0204614ed7b45
chobits <zxchobits@outlook.com>
2021-02-02 22:23:35 +0800

    php docker基础环境初始提交。php 安装pdo和redis

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<?php
+echo phpinfo();
+//$redis = new Redis();
+//$redis->connect('redis', 6379);
+//echo "Server is running: " . $redis->ping();
+//$dsn = 'mysql:dbname=test;host=mysql;port=3306';
+//$username = 'root';
+//$password = 'root';
+//    $db = new PDO($dsn, $username, $password);
+//$statement = <<<SQL
+//    	    SELECT *
+//    	    FROM `tttt`
+//SQL;
+//$sth = $db->prepare($statement);
+//$sth->execute();
+//
+///* 获取结果集中所有剩余的行 */
+//print("Fetch all of the remaining rows in the result set:\n");
+//$result = $sth->fetchAll();
+//echo "<pre>";
+//print_r($result); // phpinfo();
+//echo "</pre>";
+//exit;
+//die('safdsa');
+
